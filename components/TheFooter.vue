03ce9874142e804bb135b1e73dd780d852aebe2c
Tommy <tommyopeters@gmail.com>
2021-12-04 09:19:52 +0100

    Templating done

---
+++ components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div class="social-links">
+      <a href=""><i class="fab fa-linkedin-in"></i></a>
+      <a href=""><i class="fab fa-instagram"></i></a>
+      <a href=""><i class="fab fa-twitter"></i></a>
+      <a href=""><i class="fab fa-facebook-f"></i></a>
+      <a href=""><i class="fab fa-behance"></i></a>
+    </div>
+    <div class="copyright">2021 Sajecreate. All Rights Reserved</div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <a href="/" class="logo image-control vertical">
+      <img src="~/assets/images/logo.png" alt="" />
+    </a>
+    <nav>
+      <ul class="nav-menu">
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Bio</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
